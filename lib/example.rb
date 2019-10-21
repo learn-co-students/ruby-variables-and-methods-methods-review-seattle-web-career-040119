639a001cfcc2068126e8714b94df4d8ecd3c596a
github username <github email address>
2019-10-21 10:32:04 -0700

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+  puts "Hello World"
+end
+
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
+puts greeting
